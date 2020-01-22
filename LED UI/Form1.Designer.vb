b41d824b730baedb7d8ee53daf2ef685944ebb32
ChamodKanishka <chamodkanishka77@gmail.com>
2020-01-22 08:52:00 +0530

    crete a application for lights with visual basic

---
+++ LED UI/Form1.Designer.vb
@@ -0,0 +1,145 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container
+        Me.btnConnect = New System.Windows.Forms.Button
+        Me.btnDisconnect = New System.Windows.Forms.Button
+        Me.btnON = New System.Windows.Forms.Button
+        Me.btnOFF = New System.Windows.Forms.Button
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.lblValue = New System.Windows.Forms.Label
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox
+        Me.SuspendLayout()
+        '
+        'btnConnect
+        '
+        Me.btnConnect.Location = New System.Drawing.Point(83, 83)
+        Me.btnConnect.Name = "btnConnect"
+        Me.btnConnect.Size = New System.Drawing.Size(102, 46)
+        Me.btnConnect.TabIndex = 0
+        Me.btnConnect.Text = "Connect"
+        Me.btnConnect.UseVisualStyleBackColor = True
+        '
+        'btnDisconnect
+        '
+        Me.btnDisconnect.Location = New System.Drawing.Point(83, 150)
+        Me.btnDisconnect.Name = "btnDisconnect"
+        Me.btnDisconnect.Size = New System.Drawing.Size(102, 46)
+        Me.btnDisconnect.TabIndex = 1
+        Me.btnDisconnect.Text = "Disconnect"
+        Me.btnDisconnect.UseVisualStyleBackColor = True
+        '
+        'btnON
+        '
+        Me.btnON.Location = New System.Drawing.Point(83, 230)
+        Me.btnON.Name = "btnON"
+        Me.btnON.Size = New System.Drawing.Size(102, 46)
+        Me.btnON.TabIndex = 2
+        Me.btnON.Text = "LED ON"
+        Me.btnON.UseVisualStyleBackColor = True
+        '
+        'btnOFF
+        '
+        Me.btnOFF.Location = New System.Drawing.Point(83, 297)
+        Me.btnOFF.Name = "btnOFF"
+        Me.btnOFF.Size = New System.Drawing.Size(102, 46)
+        Me.btnOFF.TabIndex = 3
+        Me.btnOFF.Text = "LED OFF"
+        Me.btnOFF.UseVisualStyleBackColor = True
+        '
+        'SerialPort1
+        '
+        Me.SerialPort1.PortName = "COM7"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(52, 373)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(68, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "ADC Value ="
+        '
+        'lblValue
+        '
+        Me.lblValue.Location = New System.Drawing.Point(136, 373)
+        Me.lblValue.Name = "lblValue"
+        Me.lblValue.Size = New System.Drawing.Size(91, 24)
+        Me.lblValue.TabIndex = 5
+        Me.lblValue.Text = "000"
+        '
+        'Timer1
+        '
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(262, 188)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(102, 46)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(243, 69)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox1.TabIndex = 7
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(428, 431)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.lblValue)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnOFF)
+        Me.Controls.Add(Me.btnON)
+        Me.Controls.Add(Me.btnDisconnect)
+        Me.Controls.Add(Me.btnConnect)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnConnect As System.Windows.Forms.Button
+    Friend WithEvents btnDisconnect As System.Windows.Forms.Button
+    Friend WithEvents btnON As System.Windows.Forms.Button
+    Friend WithEvents btnOFF As System.Windows.Forms.Button
+    Friend WithEvents SerialPort1 As System.IO.Ports.SerialPort
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents lblValue As System.Windows.Forms.Label
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+
+End Class
---
+++ LED UI/Form1.vb
@@ -0,0 +1,45 @@
+﻿Imports System.IO.Ports
+Imports System.IO
+Public Class Form1
+
+    Private Sub btnConnect_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnConnect.Click
+        SerialPort1.PortName = ComboBox1.SelectedItem
+        SerialPort1.Open()
+        Timer1.Start()
+    End Sub
+
+    Private Sub btnDisconnect_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnDisconnect.Click
+        SerialPort1.Close()
+        Timer1.Stop()
+    End Sub
+
+    Private Sub btnON_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnON.Click
+        SerialPort1.Write("G")
+    End Sub
+
+    Private Sub btnOFF_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnOFF.Click
+        SerialPort1.Write("g")
+    End Sub
+    Dim rcv As String
+    Private Sub SerialPort1_DataReceived(ByVal sender As Object, ByVal e As System.IO.Ports.SerialDataReceivedEventArgs) Handles SerialPort1.DataReceived
+        rcv = SerialPort1.ReadLine()
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        lblValue.Text = rcv
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim port As String
+        Dim ports() As String
+
+        ports = System.IO.Ports.SerialPort.GetPortNames
+        For Each port In ports
+            ComboBox1.Items.Add(port)
+        Next
+    End Sub
+End Class
